371498570ee2e340f80f833585c59752054bf63f
SHIE, Li-Yi <lyshie@tn.edu.tw>
2017-09-09 22:18:53 +0800

    Add backlight scripts

---
+++ xbacklight-wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BRIGHT=$1
+
+if [[ $BRIGHT =~ ^inc|up$ ]]; then
+	xbacklight +10%
+fi
+
+if [[ $BRIGHT =~ ^dec|down$ ]]; then
+	xbacklight -10%
+fi
+
+CURRENT=$(printf "%.0f" $(xbacklight -get))
+notify-send -t 10 -u low -h "int:value:${CURRENT}" -i video-display "背光亮度 ${CURRENT}%"
